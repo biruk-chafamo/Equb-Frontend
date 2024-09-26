56c91c246428263ece47c40f3cc671d3086aacdf
Biruk Chafamo <chafamo.biruk@gmail.com>
2024-09-26 17:27:50 -0400

    build web

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/birukchafamo/Documents/projects/equb_v3_frontend/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/module.modulemap \
+  /Users/birukchafamo/Documents/projects/equb_v3_frontend/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
