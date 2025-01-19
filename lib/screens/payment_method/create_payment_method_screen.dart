53ba60fe7ba107885ae9ecc2c7770f434f78815c
Biruk Chafamo <birukchafamo@Biruks-MBP.home>
2025-01-19 17:14:28 -0600

    align creat payment method screen to top center

--- lib/screens/payment_method/create_payment_method_screen.dart
+++ lib/screens/payment_method/create_payment_method_screen.dart
@@ -52,57 +52,16 @@ class _CreatePaymentMethodScreenState extends State<CreatePaymentMethodScreen> {
                     .toList();
 
                 return Container(
-                  padding: AppPadding.globalPadding,
                   margin: AppMargin.globalMargin,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SectionTitleTile(
-                        'Available Services',
-                        Icons.payment,
-                        IconButton(
-                          onPressed: () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  showCloseIcon: true,
-                                  duration: Duration(seconds: 8),
-                                  content: Text(
-                                      'The selected service will be used to send and recieve payments.')),
-                            );
-                          },
-                          icon: const Icon(Icons.info_outline),
-                        ),
-                        includeDivider: false,
-                      ),
-                      Padding(
-                        padding: AppPadding.globalPadding,
-                        child: Wrap(
-                          spacing: 10.0,
-                          runSpacing: 10.0,
-                          children: List<Widget>.generate(
-                              availableServices.length, (i) {
-                            final service = availableServices[i];
-                            return ChoiceChip(
-                              selected: selectedService == service,
-                              showCheckmark: false,
-                              onSelected: (selected) {
-                                if (selected) {
-                                  setState(() {
-                                    selectedService = service;
-                                    _detailController.clear();
-                                  });
-                                }
-                              },
-                              label: Text(service),
-                            );
-                          }),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Column(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
                           SectionTitleTile(
-                            'Payment Details',
+                            'Available Services',
                             Icons.payment,
                             IconButton(
                               onPressed: () {
@@ -111,7 +70,7 @@ class _CreatePaymentMethodScreenState extends State<CreatePaymentMethodScreen> {
                                       showCloseIcon: true,
                                       duration: Duration(seconds: 8),
                                       content: Text(
-                                          'Other users will use this information to make payments to you.')),
+                                          'The selected service will be used to send and recieve payments.')),
                                 );
                               },
                               icon: const Icon(Icons.info_outline),
@@ -120,83 +79,129 @@ class _CreatePaymentMethodScreenState extends State<CreatePaymentMethodScreen> {
                           ),
                           Padding(
                             padding: AppPadding.globalPadding,
-                            child: TextFormField(
-                              controller: _detailController,
-                              inputFormatters:
-                                  selectedService == 'Bank Transfer'
-                                      ? <TextInputFormatter>[
-                                          FilteringTextInputFormatter.digitsOnly
-                                        ]
-                                      : null,
-                              style: TextStyle(
-                                fontSize: 15.0,
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onSecondaryContainer
-                                    .withOpacity(0.8), // Input text color
-                                fontWeight: FontWeight.bold,
+                            child: Wrap(
+                              spacing: 10.0,
+                              runSpacing: 10.0,
+                              children: List<Widget>.generate(
+                                  availableServices.length, (i) {
+                                final service = availableServices[i];
+                                return ChoiceChip(
+                                  selected: selectedService == service,
+                                  showCheckmark: false,
+                                  onSelected: (selected) {
+                                    if (selected) {
+                                      setState(() {
+                                        selectedService = service;
+                                        _detailController.clear();
+                                      });
+                                    }
+                                  },
+                                  label: Text(service),
+                                );
+                              }),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Column(
+                            children: [
+                              SectionTitleTile(
+                                'Payment Details',
+                                Icons.payment,
+                                IconButton(
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          showCloseIcon: true,
+                                          duration: Duration(seconds: 8),
+                                          content: Text(
+                                              'Other users will use this information to make payments to you.')),
+                                    );
+                                  },
+                                  icon: const Icon(Icons.info_outline),
+                                ),
+                                includeDivider: false,
                               ),
-                              decoration: InputDecoration(
-                                // if selected servicce is bank, hint text will be 'Bank Name', otherwise 'Equb Name'
-                                hintText: selectedService == 'Bank Transfer'
-                                    ? 'Account Number'
-                                    : 'username',
-                                hintStyle: Theme.of(context)
-                                    .textTheme
-                                    .titleMedium
-                                    ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSecondaryContainer
-                                          .withOpacity(0.5),
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15.0,
+                              Padding(
+                                padding: AppPadding.globalPadding,
+                                child: TextFormField(
+                                  controller: _detailController,
+                                  inputFormatters:
+                                      selectedService == 'Bank Transfer'
+                                          ? <TextInputFormatter>[
+                                              FilteringTextInputFormatter.digitsOnly
+                                            ]
+                                          : null,
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSecondaryContainer
+                                        .withOpacity(0.8), // Input text color
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  decoration: InputDecoration(
+                                    // if selected servicce is bank, hint text will be 'Bank Name', otherwise 'Equb Name'
+                                    hintText: selectedService == 'Bank Transfer'
+                                        ? 'Account Number'
+                                        : 'username',
+                                    hintStyle: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium
+                                        ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onSecondaryContainer
+                                              .withOpacity(0.5),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15.0,
+                                        ),
+                                    border: const UnderlineInputBorder(),
+                                    focusedBorder: const UnderlineInputBorder(),
+                                  ),
+                                  autocorrect: false,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 60),
+                          Padding(
+                            padding: AppPadding.globalPadding,
+                            child: CustomOutlinedButton(
+                              onPressed: () {
+                                if (selectedService != null &&
+                                    _detailController.text.isNotEmpty) {
+                                  if (selectedService == 'Bank Transfer' &&
+                                      _detailController.text.length != 10) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text(
+                                            'Account number must be 10 digits'),
+                                      ),
+                                    );
+                                  } else {
+                                    context.read<PaymentMethodBloc>().add(
+                                          CreatePaymentMethod(
+                                            service: selectedService!,
+                                            detail: _detailController.text,
+                                          ),
+                                        );
+                                    GoRouter.of(context).pop();
+                                  }
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text(
+                                          'Please select a service and fill in the details'),
                                     ),
-                                border: const UnderlineInputBorder(),
-                                focusedBorder: const UnderlineInputBorder(),
-                              ),
-                              autocorrect: false,
+                                  );
+                                }
+                              },
+                              child: const Text('Add a Payment Method'),
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 60),
-                      Padding(
-                        padding: AppPadding.globalPadding,
-                        child: CustomOutlinedButton(
-                          onPressed: () {
-                            if (selectedService != null &&
-                                _detailController.text.isNotEmpty) {
-                              if (selectedService == 'Bank Transfer' &&
-                                  _detailController.text.length != 10) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text(
-                                        'Account number must be 10 digits'),
-                                  ),
-                                );
-                              } else {
-                                context.read<PaymentMethodBloc>().add(
-                                      CreatePaymentMethod(
-                                        service: selectedService!,
-                                        detail: _detailController.text,
-                                      ),
-                                    );
-                                GoRouter.of(context).pop();
-                              }
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Please select a service and fill in the details'),
-                                ),
-                              );
-                            }
-                          },
-                          child: const Text('Add a Payment Method'),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 );
               } else {
